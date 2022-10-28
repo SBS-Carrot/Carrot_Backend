1e6d27daaa56953c734c816e137cee8b9bb71268
hyelim3 <shl0703ss@gmail.com>
2022-10-28 18:30:21 +0900

    ad

--- src/main/java/com/carrot/backend/productImage/controller/ProductImageController.java
+++ src/main/java/com/carrot/backend/productImage/controller/ProductImageController.java
@@ -1,16 +1,9 @@
 package com.carrot.backend.productImage.controller;
 
 import com.carrot.backend.product.Service.ProductService;
-import com.carrot.backend.product.domain.Product;
 import com.carrot.backend.productImage.Service.ProductImageService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
 
 @RestController
 @AllArgsConstructor
@@ -19,12 +12,7 @@ public class ProductImageController {
     private final ProductImageService productImageService;
 
 
-    @PostMapping("/createProductImages/{productId}")
 
-    public ProductImageDto uploadProfilePhoto(@PathVariable ("productId") long productId, @RequestParam("files") MultipartFile multipartFile) throws IOException {
-
-        return productImageService.upload(productId, multipartFile, "images");
-    }
 
 
 
