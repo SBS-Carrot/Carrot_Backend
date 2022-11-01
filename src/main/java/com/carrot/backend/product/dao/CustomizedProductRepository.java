a5415e03d274906688ba860fcca4f8adf2baa4c8
byunghoonyoon <qudgns0218@gmail.com>
2022-11-01 17:45:31 +0900

    a

--- src/main/java/com/carrot/backend/product/dao/CustomizedProductRepository.java
+++ src/main/java/com/carrot/backend/product/dao/CustomizedProductRepository.java
@@ -1,7 +1,5 @@
 package com.carrot.backend.product.dao;
 
-import com.carrot.backend.product.domain.Product;
-
 public interface CustomizedProductRepository {
-    Product getProduct(Integer id);
+
 }
