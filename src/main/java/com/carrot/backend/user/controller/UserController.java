08feab58aaa7e6c7a531c196c3afdd42ed82f094
byunghoonyoon <qudgns0218@gmail.com>
2022-11-14 02:04:33 +0900

    프로필 업로드 하는중, 값 못받음

--- src/main/java/com/carrot/backend/user/controller/UserController.java
+++ src/main/java/com/carrot/backend/user/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.carrot.backend.user.controller;
 
+import com.carrot.backend.product.dto.ProductDto;
 import com.carrot.backend.user.UserLoginForm;
 import com.carrot.backend.user.domain.User;
 import com.carrot.backend.user.dto.UserDto;
@@ -7,6 +8,10 @@ import com.carrot.backend.user.service.UserService;
 import com.carrot.backend.util.NoUserException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
 
 
 @RestController
@@ -15,16 +20,17 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/createUser")
-    public User createUser(@RequestBody UserDto userDto){
+    public User createUser(@RequestBody UserDto userDto) {
         User user = userService.create(userDto);
         return user;
     }
 
     @PostMapping("/loginUser")
-    public String loginUser (@RequestBody UserLoginForm userLoginForm){
+    public String loginUser(@RequestBody UserLoginForm userLoginForm) {
         String userid = userService.login(userLoginForm);
         return userid;
     }
+
     @GetMapping("/getUser/{userid}")
     public User _getUser(@PathVariable("userid") String userid) throws NoUserException {
         User user = userService.getUser(userid);
@@ -32,9 +38,14 @@ public class UserController {
     }
 
     @GetMapping("/checkId/{userid}")
-    public boolean _checkId(@PathVariable("userid") String uid){
+    public boolean _checkId(@PathVariable("userid") String uid) {
         boolean id = userService.checkId(uid);
         return id;
     }
 
+    @PostMapping("/userProfileImageChange")
+    public void changeUserProfile(@RequestPart(value = "userdto") UserDto userdto, @RequestPart("file") List<MultipartFile> multipartFile) throws IOException {
+        userService.changeUserProfileImage(userdto, multipartFile, "users");
+
+    }
 }
--- src/main/java/com/carrot/backend/user/service/UserService.java
+++ src/main/java/com/carrot/backend/user/service/UserService.java
@@ -1,24 +1,45 @@
 package com.carrot.backend.user.service;
 
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.carrot.backend.productImage.domain.ProductImages;
+import com.carrot.backend.productImage.dto.ProductImageDto;
 import com.carrot.backend.user.UserLoginForm;
 import com.carrot.backend.user.dao.UserRepository;
 import com.carrot.backend.user.domain.User;
 import com.carrot.backend.user.dto.UserDto;
 import com.carrot.backend.util.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
-@RequiredArgsConstructor
 @Service
+@Slf4j
+@RequiredArgsConstructor
+@Component
 public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
+    private final AmazonS3Client amazonS3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
 
     public User create(UserDto userDto) {
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
@@ -103,4 +124,81 @@ public class UserService {
         }
 
     }
-}
+
+    public void changeUserProfileImage(UserDto userdto, List<MultipartFile> multipartFile, String users) {
+        List<File> uploadFile = new ArrayList<File>();
+        for(int i=0;i< multipartFile.size();i++) {
+
+
+            MultipartFile files = multipartFile.get(i);
+            File upload = null;
+            try {
+                upload = convert(files)
+                        .orElseThrow(() -> new IllegalArgumentException("MultipartFile -> File로 전환이 실패했습니다."));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            uploadFile.add(upload);
+        }
+
+
+
+
+
+        }
+
+
+
+        private void upload (UserDto userdto, List<File> uploadFile, String dirName){
+                String userid = userdto.getUserid();
+                String fileName = dirName + "/" + UUID.randomUUID() + uploadFile.get(0).getName();
+
+
+                String path = putS3(uploadFile.get(0), fileName);
+
+                removeNewFile(uploadFile.get(0));
+                User user = userRepository.findByUserid(userid).orElseThrow(()-> new DataNotFoundException("user not found"));
+
+                user.setProfileImage(path);
+                user.setNickname(userdto.getNickname());
+                userRepository.save(user);
+
+
+
+        }
+
+        private String putS3 (File uploadFile, String fileName){
+            amazonS3Client.putObject(new PutObjectRequest(bucket, fileName, uploadFile).withCannedAcl(
+                    CannedAccessControlList.PublicRead));
+
+
+            return amazonS3Client.getUrl(bucket, fileName).toString();
+        }
+
+        private void removeNewFile (File targetFile){
+            if (targetFile.delete()) {
+                log.info("파일이 삭제되었습니다.");
+            } else {
+                log.info("파일이 삭제되지 못했습니다.");
+            }
+        }
+
+        private Optional<File> convert (MultipartFile file) throws IOException {
+
+
+            File convertFile = new File(file.getOriginalFilename());
+
+            if (convertFile.createNewFile()) {
+
+                try (FileOutputStream fos = new FileOutputStream(convertFile)) {
+                    fos.write(file.getBytes());
+                }
+                return Optional.of(convertFile);
+            }
+
+            return Optional.empty();
+        }
+
+
+    }
+
